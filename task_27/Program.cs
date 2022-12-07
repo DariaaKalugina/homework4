c4a05d312653cdcfae43cc2a76ff93466f4a2e59
Daria <macdaria1999@gmail.com>
2022-12-07 22:43:28 +0300

    Add task 27

---
+++ task_27/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToString(n).Length;
+int s = 0;
+for (int i = 0; i < c; i++)
+ {
+    int num = n % 10;
+    n = n/10;
+    s = s + num;
+ }
+ Console.WriteLine($"Сумма цифр в числе: {s} " );
