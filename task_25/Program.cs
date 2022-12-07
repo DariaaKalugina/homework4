beb3be72a73bd847b8822fd4503f4ae65ef5de0d
Daria <macdaria1999@gmail.com>
2022-12-07 22:15:20 +0300

    Add task 25

---
+++ task_25/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int d = a;
+for (int i = 1; i < b; i++)
+{
+    d = d * a;
+}
+Console.WriteLine ($"А в степени В: {d} ");
